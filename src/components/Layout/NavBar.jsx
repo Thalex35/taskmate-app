53a210fd48a3d3f006b28ab0b9b56ecf4c6e6d44
Pressaille Rosar <pressaillerosar@gmail.com>
2026-04-22 10:41:08 -0700

    ajout Menu.css

--- src/components/Layout/NavBar.jsx
+++ src/components/Layout/NavBar.jsx
@@ -1,11 +1,35 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import "../../styles/NavBar.css"
 
 export default function NavBar() {
+
+  const[modeSombre, setModeSombre] = useState(false);
+
   return (
     <nav className="navbar">
-      <Link to="/">TaskMate</Link>
-      <Link to="/">Mode Sombre</Link>
-      <Link to="/Devoirs">+ Nouveau Devoir </Link>
+      <Link to="/dashboard"
+      className="navbar-logo">
+        <span className="logo-task">Task</span>
+      </Link>
+
+
+
+      <div className="navbar-action">
+        <button>
+          className="navbar-btn-mode"
+          onClick={() => setModeSombre
+          (!modeSombre)}
+
+          {modeSombre ? "Mode clair" : "Mode sombre"}
+        </button>
+
+        <Link to= "/devoir/nouveau"
+        className="navbar-btn-nouveau">
+          + Nouveau Devoir
+        </Link>
+      </div>
+      
     </nav>
   );
 }
